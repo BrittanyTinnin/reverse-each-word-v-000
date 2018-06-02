d702ef1fa1183d3f2f7e50d91f9c6d4d33a34b2f
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-02 19:08:51 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(sentence1)
+  sentence1.to_s
+  sentence1.each do |word|
+    word.reverse
+  end
+end
+
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   sentence.each do |word|
+#     new_sentence << word.reverse
+#   end
+#     new_sentence
+# end
