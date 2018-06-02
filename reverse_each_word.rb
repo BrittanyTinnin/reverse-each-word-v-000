f3014ea4f5577a2867b755f047930c7e280cb714
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-02 21:51:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,7 +13,7 @@ def reverse_each_word(sentence)
   sentence1.collect do |word|
     word.reverse
   end
-  sentence1.join
+  sentence1.join(" ")
 end
 # def reverse_each_word(sentence)
 #   new_sentence = []
